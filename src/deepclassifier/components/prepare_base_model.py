5639cf504ea37b3b9a647c3c7ad06998d268b915
4ST4ROTH <singhmohan1998july@gmail.com>
2022-09-30 22:21:07 +0530

    data ingestion,base model and callbacks completed

--- src/deepclassifier/components/prepare_base_model.py
+++ src/deepclassifier/components/prepare_base_model.py
@@ -5,19 +5,20 @@ from deepclassifier.entity import PrepareBaseModelConfig
 
 
 class PrepareBaseModel:
-    def __init__(self, config: PrepareBaseModelConfig):
+    def __init__ (self, config:PrepareBaseModelConfig):
         self.config = config
 
     def get_base_model(self):
         self.base_model = tf.keras.applications.vgg16.VGG16(
-            weights=self.config.params_weight,
-            input_shape=self.config.params_imgae_size,
-            include_top=self.config.params_include_top,
+            weights= self.config.params_weights,
+            input_shape=self.config.params_image_size,
+            include_top=self.config.params_include_top
         )
 
-        self.save_model(path=self.config.base_model_path, model=self.base_model)
+        self.save_model(path=self.config.base_model_path,model=self.base_model)
 
-    def _prepare_full_model(model, classes, learnng_rate, freeze_all, freeze_till):
+    @staticmethod
+    def _prepare_full_model(model,classes,learnng_rate,freeze_all,freeze_till):
         if freeze_all:
             for layer in model.layers:
                 model.trainable = False
@@ -26,16 +27,20 @@ class PrepareBaseModel:
                 model.trainable = False
 
         flatten_in = tf.keras.layers.Flatten()(model.output)
-        prediction = tf.keras.layers.Dense(units=classes, activation="softmax")(
-            flatten_in
+        prediction = tf.keras.layers.Dense(
+            units=classes,
+            activation="softmax"
+        )(flatten_in)
+
+        full_model = tf.keras.models.Model(
+            inputs=model.input,
+            outputs=prediction
         )
 
-        full_model = tf.keras.models.Model(inputs=model.input, outputs=prediction)
-
         full_model.compile(
-            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+            optimizer=tf.keras.optimizers.SGD(learning_rate=learnng_rate),
             loss=tf.keras.losses.CategoricalCrossentropy(),
-            metrics=["accuracy"],
+            metrics=["accuracy"]
         )
 
         full_model.summary()
@@ -43,15 +48,18 @@ class PrepareBaseModel:
 
     def update_base_model(self):
         self.full_model = self._prepare_full_model(
-            model=self.model,
+            model=self.base_model,
             classes=self.config.params_classes,
             freeze_all=True,
             freeze_till=None,
-            learning_rate=self.config.params_learning_rate,
+            learnng_rate=self.config.params_learning_rate
+        
         )
 
         self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+        
+    
 
     @staticmethod
-    def save_model(path: Path, model: tf.keras.Model):
+    def save_model(path:Path , model : tf.keras.Model):
         model.save(path)
